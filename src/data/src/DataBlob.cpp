9c5256c37b1ddd73a41785a28c35eca01b41a26f
Fred Dulwich <fred@orion.(none)>
2009-12-03 15:51:59 +0000

    Added timestamp functionality to DataBlob.

--- src/data/src/DataBlob.cpp
+++ src/data/src/DataBlob.cpp
@@ -1,16 +1,48 @@
 #include "DataBlob.h"
-
+#include <QDateTime>
+#include <QDate>
+#include <QTime>
 
 namespace pelican {
 
-
-// class DataBlob 
+/**
+ * @details
+ * Constructs a new data blob.
+ */
 DataBlob::DataBlob()
+{
+    /* Set the default time stamp */
+    setTimeStamp();
+}
+
+/**
+ * @details
+ * Data blob destructor.
+ */
+DataBlob::~DataBlob()
 {
 }
 
-DataBlob::~DataBlob()
+/**
+ * @details
+ * Sets the timestamp of the data blob using the Modified Julian Date
+ * from the system clock, converted to UTC.
+ * Note that MJD = JD - 2400000.5, and starts from midnight
+ * rather than midday.
+ */
+void DataBlob::setTimeStamp()
 {
+    /* Get the system date */
+    QDateTime utc = QDateTime::currentDateTime().toUTC();
+
+    /* Set the date part of the MJD */
+    _modifiedJulianDate = utc.date().toJulianDay();
+    _modifiedJulianDate -= 2400000.5;
+
+    /* Set the time part of the MJD */
+    QTime midnight(0, 0, 0);
+    double millisec = midnight.msecsTo(utc.time());
+    _modifiedJulianDate += (millisec / 86400000.0);
 }
 
 } // namespace pelican
