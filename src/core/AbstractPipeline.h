05d36fa278c353d796c99ba4ad8e8190197ac80c
Fred Dulwich <fred@orion.(none)>
2010-01-14 18:59:45 +0000

    AbstractPipeline now knows about the application module factory and can create pipeline modules, and will return the data required to run them.

--- src/core/AbstractPipeline.h
+++ src/core/AbstractPipeline.h
@@ -2,44 +2,64 @@
 #define ABSTRACTPIPELINE_H
 
 #include "data/DataRequirements.h"
+#include <QList>
 
 /**
  * @file AbstractPipeline.h
  */
 
+namespace pelican {
+
+class AbstractModule;
+class ModuleFactory;
+class DataBlob;
+
 /**
  * @class AbstractPipeline
  *  
  * @brief
+ * Base class for all Pelican pipeline objects.
  * 
  * @details
+ * This is the base class for all Pelican pipelines, which act as containers
+ * for pipeline modules.
  * 
+ * Inherit from this class and implement the init() and run() virtual methods
+ * to create a new pipeline.
  */
-
-namespace pelican {
-
-class DataBlob;
-
 class AbstractPipeline
 {
+    private:
+        /// Pointer to the PipelineApplication module factory.
+        ModuleFactory* _moduleFactory;
+
+        /// List of pointers to modules in this pipeline.
+        QList<AbstractModule*> _modules;
+
+    protected:
+        /// The service and stream data required by the pipeline.
+        DataRequirements _data;
+
+        /// Create a pipeline module using the module factory.
+        AbstractModule* createModule(const QString& moduleName);
+
     public:
-        AbstractPipeline(  );
+        /// Constructs a new abstract pipeline.
+        AbstractPipeline();
+
+        /// Destroys the abstract pipeline.
         ~AbstractPipeline();
 
         /// Return the data requirements for the pipeline.
-        const DataRequirements& dataRequired() const;
+        const DataRequirements& dataRequired();
 
-        /// Initialises the pipeline.
-        virtual void init() = 0;
+        /// Initialises the pipeline by creating the required modules.
+        virtual void init() {}
 
-        /// Runs the pipeline.
+        /// Defines a single iteration of the pipeline (pure virtual).
         virtual void run(QHash<QString, DataBlob*>& dataHash) = 0;
-
-    protected:
-        DataRequirements _data;
-
-    private:
 };
 
 } // namespace pelican
+
 #endif // ABSTRACTPIPELINE_H 
--- src/core/src/AbstractPipeline.cpp
+++ src/core/src/AbstractPipeline.cpp
@@ -1,4 +1,8 @@
-#include "AbstractPipeline.h"
+#include "core/AbstractPipeline.h"
+#include "core/ModuleFactory.h"
+#include "core/PipelineApplication.h"
+#include "modules/AbstractModule.h"
+#include <QtGlobal>
 #include "utility/memCheck.h"
 
 namespace pelican {
@@ -9,6 +13,13 @@ namespace pelican {
  */
 AbstractPipeline::AbstractPipeline()
 {
+    /* Store pointer to module factory */
+    _moduleFactory = PipelineApplication::moduleFactory();
+    if (_moduleFactory == NULL)
+        throw QString("Module factory doesn't exist: Create a PipelineApplication first.");
+
+    /* Call init() to create the pipeline modules */
+    init();
 }
 
 /**
@@ -21,12 +32,29 @@ AbstractPipeline::~AbstractPipeline()
 
 /**
  * @details
- * Return the data requirements for the pipeline.
+ * Creates a new module using the application's module factory.
  */
-const DataRequirements& AbstractPipeline::dataRequired() const
+AbstractModule* AbstractPipeline::createModule(const QString& moduleName)
 {
+    AbstractModule* module = _moduleFactory->createModule(moduleName);
+    _modules.append(module);
+    return module;
+}
+
+/**
+ * @details
+ * Computes and returns the data required by the modules in this pipeline.
+ */
+const DataRequirements& AbstractPipeline::dataRequired()
+{
+    /* Clear existing data requirements */
+    _data.clear();
+
+    /* Loop over all the modules in the pipeline */
+    foreach (AbstractModule* module, _modules) {
+        _data += module->requiredData();
+    }
     return _data;
 }
 
 } // namespace pelican
-
