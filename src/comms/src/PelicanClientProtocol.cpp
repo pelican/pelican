b7806fab23b987976d1d602ba41e8a7c0c7f8e7f
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2010-01-29 17:27:59 +0000

    fix memory leak in test

--- src/comms/src/PelicanClientProtocol.cpp
+++ src/comms/src/PelicanClientProtocol.cpp
@@ -1,6 +1,7 @@
 #include "PelicanClientProtocol.h"
 #include "ServerRequest.h"
 #include <QByteArray>
+#include <QDataStream>
 
 #include "utility/memCheck.h"
 
@@ -20,6 +21,9 @@ PelicanClientProtocol::~PelicanClientProtocol()
 QByteArray PelicanClientProtocol::serialise(const ServerRequest& req)
 {
     QByteArray data;
+    QDataStream ds(&data, QIODevice::WriteOnly);
+    ds.setVersion(QDataStream::Qt_4_0);
+    ds << req.type();
     switch(req.type()) {
         case ServerRequest::Acknowledge:
             break;
