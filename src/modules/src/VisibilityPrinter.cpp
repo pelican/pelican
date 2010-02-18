3ef5aa8f2668a45ffd647e3d869a2d8dc4692be6
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-02-18 18:23:16 +0000

    VisibilityPrinter now prints real loaded visibilities.

--- src/modules/src/VisibilityPrinter.cpp
+++ src/modules/src/VisibilityPrinter.cpp
@@ -27,22 +27,26 @@ VisibilityPrinter::~VisibilityPrinter()
 
 void VisibilityPrinter::run(QHash<QString, DataBlob*>& data)
 {
-//    _vis = static_cast<VisibilityData*>(data["VisibilityData"]);
-//    if (!_vis) throw QString("Data blob missing: Visibility Data");
-//    if (_vis->nAntennas() == 0) throw QString("Empty data blob: Visibility Data");
+    std::cout << "Hello from VisibilityPrinter::run() method!\n";
+
+    _vis = static_cast<VisibilityData*>(data["VisibilityData"]);
+    if (!_vis) throw QString("Data blob missing: Visibility Data");
+    if (_vis->nAntennas() == 0) throw QString("Empty data blob: Visibility Data");
 
 //    complex_t* vis = _vis->ptr();
-//    unsigned nChan = _vis->nChannels();
-//    unsigned nAnt = _vis->nAntennas();
-//    unsigned nPol = _vis->nPolarisations();
-//    for (unsigned p = 0; p < nPol; p++) {
-//        for (unsigned c = 0; c < nChan; c++) {
-//            for (unsigned j = 0; j < nAnt; j++) {
-//                for (unsigned i = 0; i < nAnt; i++) {
-//            }
-//        }
-//    }
-    std::cout << "Hello from VisibilityPrinter::run() method!\n";
+    unsigned nChan = _vis->nChannels();
+    unsigned nAnt = _vis->nAntennas();
+    unsigned nPol = _vis->nPolarisations();
+    for (unsigned p = 0; p < nPol; p++) {
+        for (unsigned c = 0; c < nChan; c++) {
+            for (unsigned j = 0; j < nAnt; j++) {
+                for (unsigned i = 0; i < nAnt; i++) {
+                    std::cout << "VisData: " << (*_vis)(i, j, c, p) << std::endl;
+                }
+            }
+        }
+    }
+
 }
 
 
