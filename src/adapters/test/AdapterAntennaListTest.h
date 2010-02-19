0d13050fd2cd85c95e24a17ea32fa3a9b4b3cbb8
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-02-19 12:31:09 +0000

    Added an antenna list adapter test and associcated test data.

---
+++ src/adapters/test/AdapterAntennaListTest.h
@@ -0,0 +1,42 @@
+#ifndef ADAPTERANTENNALISTTEST_H
+#define ADAPTERANTENNALISTTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+/**
+ * @file AdapterAntennaListTest.h
+ */
+
+namespace pelican {
+
+/**
+ * @class AdapterAntennaListTest
+ *
+ * @brief
+ *
+ * @details
+ */
+
+class AdapterAntennaListTest : public CppUnit::TestFixture
+{
+    public:
+        CPPUNIT_TEST_SUITE( AdapterAntennaListTest );
+        CPPUNIT_TEST( test_method );
+        CPPUNIT_TEST_SUITE_END();
+
+    public:
+        void setUp();
+        void tearDown();
+
+        // Test Methods
+        void test_method();
+
+    public:
+        AdapterAntennaListTest( );
+        ~AdapterAntennaListTest();
+
+    private:
+};
+
+} // namespace pelican
+#endif // ADAPTERANTENNALISTTEST_H
