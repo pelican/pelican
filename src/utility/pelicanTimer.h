e1293f67a0a3e232385d1e6a2d22709a04127f6a
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-02-23 16:27:54 +0000

    Fixed timer to use real wall-clock time instead of CPU clocks...

--- src/utility/pelicanTimer.h
+++ src/utility/pelicanTimer.h
@@ -1,6 +1,7 @@
 #ifndef PELICANTIMER_H_
 #define PELICANTIMER_H_
 
+#include <QTime>
 #include <ctime>
 #include <cstdio>
 
@@ -15,18 +16,32 @@
  * Define TIMER_ENABLE to use them.
  *
  * Call TIMER_START to start the timer, and TIMER_STOP(...) to print the time
- * taken (with a message prefix) to standard output (STDOUT).
+ * taken (with a message prefix) to standard output (STDOUT). The timer uses
+ * QTime and therefore has millisecond accuracy.
+ *
+ * To use a CPU clock timer instead, call CLOCK_START and CLOCK_STOP(...)
+ * instead.
  */
 #ifdef TIMER_ENABLE
-#define TIMER_START {clock_t _pelican_start_time = clock();
-#define TIMER_STOP(...) clock_t _pelican_timer_interval = clock() - _pelican_start_time; \
+#define TIMER_START {QTime _t; _t.start();
+#define TIMER_STOP(...) fprintf(stdout, "\n"); \
+    fprintf(stdout, __VA_ARGS__); \
+    fprintf(stdout, ": %.2f sec.\n", _t.elapsed() / 1000.0);}
+#else
+#define TIMER_START ;
+#define TIMER_STOP(...) ;
+#endif
+
+#ifdef TIMER_ENABLE
+#define CLOCK_START {clock_t _pelican_start_time = clock();
+#define CLOCK_STOP(...) clock_t _pelican_timer_interval = clock() - _pelican_start_time; \
     float _pelican_time_sec = (float)(_pelican_timer_interval) / CLOCKS_PER_SEC; \
     fprintf(stdout, "\n"); \
     fprintf(stdout, __VA_ARGS__); \
     fprintf(stdout, ": %.2f sec.\n", _pelican_time_sec);}
 #else
-#define TIMER_START ;
-#define TIMER_STOP(...) ;
+#define CLOCK_START ;
+#define CLOCK_STOP(...) ;
 #endif
 
 #endif /* PELICANTIMER_H_ */
