b49159f57db6aa696970be2582abb6f3deac3b1b
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-03-02 18:21:38 +0000

    Tidied up interface to astrometry transform.

--- src/data/SiteData.h
+++ src/data/SiteData.h
@@ -22,14 +22,14 @@ namespace pelican {
  * - Site geodetic longitude in radians (east-positive).
  * - Site geodetic latitude in radians.
  * - Cosine and sine of site geodetic latitude.
- * - Site altitude above sea level in metres.
+ * - The UT1 - UTC offset, in seconds.
  * - Atmospheric pressure in millibars.
+ * - Site altitude above sea level in metres.
  * - Atmospheric temperature in Kelvin.
  * - Normalised relative humidity.
  * - Wavelength of observation in microns.
  * - Tropospheric lapse rate in Kelvin/metre.
  * - The x- and y-components of polar motion.
- * - The UT1 - UTC offset, in seconds.
  */
 class SiteData
 {
@@ -37,14 +37,14 @@ class SiteData
         double longitude;     ///< Site geodetic longitude in radians.
         double latitude;      ///< Site geodetic latitude in radians.
         Vector2d lat;         ///< Cosine and sine of site geodetic latitude.
-        double altitude;      ///< Site altitude above sea level in metres.
+        double deltaUT;       ///< The UT1 - UTC offset, in seconds.
         double pressure;      ///< Atmospheric pressure in millibars.
         double temperature;   ///< Atmospheric temperature in Kelvin.
         double humidity;      ///< Normalised relative humidity.
         double wavelength;    ///< Wavelength of observation in microns.
         double lapseRate;     ///< Tropospheric lapse rate in Kelvin/metre.
+        double altitude;      ///< Site altitude above sea level in metres.
         Vector2d polarMotion; ///< The x- and y-components of polar motion.
-        double deltaUT;       ///< The UT1 - UTC offset, in seconds.
 };
 
 } // namespace pelican
