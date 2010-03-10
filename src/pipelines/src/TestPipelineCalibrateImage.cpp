68051d55862e7e3f880dd33db64fa5871807f443
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-03-10 21:06:23 +0000

    Renamed TestPipelineDirtyImage.cpp to TestPipelineCalibrateImage.cpp

--- src/pipelines/src/TestPipelineCalibrateImage.cpp
+++ src/pipelines/src/TestPipelineCalibrateImage.cpp
@@ -42,10 +42,10 @@ TestPipelineCalibrateImage::~TestPipelineCalibrateImage()
 void TestPipelineCalibrateImage::init()
 {
     setName("TestPipelineDirtyImage");
-    _imager = static_cast<ZenithImagerDft*>(createModule("ZenithImagerDft"));
-    _fitsWriter = static_cast<ImageWriterFits*>(createModule("ImageWriterFits"));
-    _modelGen = static_cast<ZenithModelVisibilities*>(createModule("ZenithModelVisibilities"));
-    _calibrate = static_cast<ZenithCalibrater*>(createModule("ZenithCalibrater"));
+    _imager = (ZenithImagerDft*) createModule("ZenithImagerDft");
+    _fitsWriter = (ImageWriterFits*) createModule("ImageWriterFits");
+    _modelGen = (ZenithModelVisibilities*) createModule("ZenithModelVisibilities");
+    _calibrate = (ZenithCalibrater*) createModule("ZenithCalibrater");
 
     // Requests for remote data to be inserted in the data hash.
     requestRemoteData("AntennaPositions");
@@ -62,20 +62,18 @@ void TestPipelineCalibrateImage::init()
  */
 void TestPipelineCalibrateImage::run(QHash<QString, DataBlob*>& remoteData)
 {
-    _antPos = static_cast<AntennaPositions*>(remoteData["AntennaPositions"]);
-    _rawVis = static_cast<VisibilityData*>(remoteData["VisibilityData"]);
+    AntennaPositions* antPos = (AntennaPositions*) remoteData["AntennaPositions"];
+    VisibilityData* rawVis = (VisibilityData*) remoteData["VisibilityData"];
 
-    _modelVis->setTimeStamp(_rawVis->timeStamp());
-    _modelGen->run(_antPos, _modelVis);
+    _modelVis->setTimeStamp(rawVis->timeStamp());
+    _modelGen->run(antPos, _modelVis);
 
-    _calibrate->run(_rawVis, _modelVis, _correctedVis);
+    _calibrate->run(rawVis, _modelVis, _correctedVis);
 
-    _imager->run(_image, _antPos, _correctedVis);
+    _imager->run(_image, antPos, _correctedVis);
     _fitsWriter->run(_image);
 
     stop();
 }
-
-
 
 } // namespace pelican
