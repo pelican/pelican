7d1ee24b8c81220171e0c5628cb3ba5965e4365d
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2010-03-13 04:26:28 +0000

    added a method in the TestConfig class to return command line arguments char** argv for a test xml config file

--- src/utility/TestConfig.h
+++ src/utility/TestConfig.h
@@ -31,8 +31,44 @@ namespace pelican {
 class TestConfig : public Config
 {
     public:
-        /// Constructs a test configuration reading the
-        TestConfig(const QString fileName, QString package) : Config()
+        /// Constructs a test configuration object for the test configuration
+        /// file of name /p fileName in the specified pelican pacakge folder
+        /// /p package
+        TestConfig(const QString& fileName, const QString& package) : Config() {
+            read(findTestFile(fileName, package));
+            _argv = NULL;
+        }
+
+        /// Constructs an empty test configuration object.
+        TestConfig() : Config() {
+            _argv = NULL;
+        }
+
+        /// Destroys the test configuration.
+        ~TestConfig() {
+            if (_argv) free(_argv);
+        }
+
+    public:
+        /// Returns command line options for a given XML filename and package.
+        char** argv(const QString& fileName, const QString& package)
+        {
+            if (_argv) free(_argv);
+            QString path = findTestFile(fileName, package);
+            QStringList args = QStringList() << "pelican" << "--config=" + path;
+            int argc = args.count();
+            _argv = (char**) malloc(argc * sizeof(char*));
+            for (int i = 0; i < argc; i++) {
+                _argv[i] = (char*) malloc(args[i].size() * sizeof(char) + 1);
+                strcpy(_argv[i], args[i].toLatin1().data());
+            }
+            return _argv;
+        }
+
+    public:
+        /// Returns the file path to a test file /p fileName for the specified
+        /// package library /p package.
+        static QString findTestFile(const QString& fileName, const QString& package)
         {
             QStringList searchPaths;
             searchPaths << "data";
@@ -42,16 +78,16 @@ class TestConfig : public Config
             searchPaths << "../../" + package + "/test/data";
             searchPaths << "../../../" + package + "/test/data";
             QDir::setSearchPaths("testData", searchPaths);
-
             QString name = "testData:" + fileName;
             QFile file(name);
             if (!file.exists()) {
                 throw QString("TestConfig: Configuration file %1 not found.").arg(fileName);
             }
-            read(QFileInfo(file).absoluteFilePath());
+            return QFileInfo(file).absoluteFilePath();
         }
 
-        ~TestConfig() {}
+    private:
+        char** _argv;
 };
 
 } // pelican
