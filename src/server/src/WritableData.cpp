e5213623f1fd64eddf8158dd98f60ea35364342d
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2010-03-17 17:17:40 +0000

    add offset option to write method

--- src/server/src/WritableData.cpp
+++ src/server/src/WritableData.cpp
@@ -26,10 +26,10 @@ LockableData* WritableData::data()
     return _data;
 }
 
-void WritableData::write(const void* buf, size_t size)
+void WritableData::write(const void* buf, size_t size, size_t offset )
 {
-    Q_ASSERT( size <= _data->data()->size() );
-    std::memcpy(_data->data()->operator*(), buf, size);
+    Q_ASSERT( size + offset <= _data->data()->size() );
+    std::memcpy(_data->data()->operator*(), buf + offset , size);
 }
 
 bool WritableData::isValid() const
