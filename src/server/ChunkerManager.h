e17094a1dfe0121780943a634e7aa42295ac5a8c
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2010-03-26 18:19:11 +0000

    add ChunkerManager

---
+++ src/server/ChunkerManager.h
@@ -0,0 +1,55 @@
+#ifndef CHUNKERMANAGER_H
+#define CHUNKERMANAGER_H
+
+#include <QSet>
+#include <QMap>
+#include <QString>
+#include <QPair>
+
+/**
+ * @file ChunkerManager.h
+ */
+
+namespace pelican {
+    class AbstractChunker;
+    class DataManager;
+
+/**
+ * @class ChunkerManager
+ *  
+ * @brief
+ *    Manage and operatoe chunkers
+ * @details
+ *    Anything comming througbh a Stream Data chunker
+ *    will be associated with the current version of service data
+ *    service data has no such association
+ */
+
+class ChunkerManager
+{
+    public:
+        ChunkerManager( );
+        ~ChunkerManager();
+
+        /// initialise the passed datamanager to support the contained chunkers
+        void init(DataManager& dm);
+
+        /// add a Chunker as a Stream Chunker
+        //  This will take ownership of the chunker
+        //  (see above for more info)
+        void addStreamChunker( AbstractChunker* );
+        /// add a Chunker as a Service Chunker
+        //  This will take ownership of the chunker
+        void addServiceChunker( AbstractChunker* );
+
+    private:
+        void _addChunker( AbstractChunker* chunker);
+
+    private:
+        QMap<QPair<QString,quint16>,AbstractChunker* > _chunkerPortMap;
+        QSet<QString> _streamDataTypes;
+        QSet<QString> _serviceDataTypes;
+};
+
+} // namespace pelican
+#endif // CHUNKERMANAGER_H 
