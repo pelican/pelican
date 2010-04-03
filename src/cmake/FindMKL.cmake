1bf9383f527cd7e41e9308caac394613936f0ba2
Benjamin Mort <ben.mort@gmail.com>
2010-04-03 13:13:15 +0100

    updated cmake lists to find the mkl headers (was previously assuming they were in the system path). Added cmake macro to find FFTW (needed for polyphase channeliser)

--- src/cmake/FindMKL.cmake
+++ src/cmake/FindMKL.cmake
@@ -1,69 +1,86 @@
-# - Find the MKL libraries (no includes)
+# Find Intel MKL.
+################################################################################
 # This module defines
-#  MKL_LIBRARIES, the libraries needed to use Intel's implementation of BLAS & LAPACK.
-#  MKL_FOUND, If false, do not try to use MKL.
+#  MKL_FOUND:     If false, do not try to use MKL.
+#  MKL_LIBRARIES: The libraries needed to use MKL BLAS & LAPACK.
+#  MKL_INCLUDE_DIR:
+################################################################################
+
 
 # Architecture specfic interface layer
 # ==============================================================================
-IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
-  SET(MKL_NAMES ${MKL_NAMES} mkl_intel_lp64)
-ELSE(CMAKE_SIZEOF_VOID_P EQUAL 8)
-  SET(MKL_NAMES ${MKL_NAMES} mkl_intel)
-ENDIF(CMAKE_SIZEOF_VOID_P EQUAL 8)
+if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set(MKL_NAMES ${MKL_NAMES} mkl_intel_lp64)
+else (CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set(MKL_NAMES ${MKL_NAMES} mkl_intel)
+endif (CMAKE_SIZEOF_VOID_P EQUAL 8)
+
 
 # Computation layer:  see http://bit.ly/bMCczV
 # ==============================================================================
-SET(MKL_NAMES ${MKL_NAMES} mkl_core)
-#SET(MKL_NAMES ${MKL_NAMES} mkl_lapack)
-
+set(MKL_NAMES ${MKL_NAMES} mkl_core)
+#SET(MKL_NAMES ${MKL_NAMES} mkl_lapack) # only needed for extended LAPACK functions.
 # Library dispatcher for dynamic load of processor specific kernel
 #SET(MKL_NAMES ${MKL_NAMES} mkl)
+
 
 # Threading model
 # ==============================================================================
 #SET(MKL_THREADED true)
 
-IF (MKL_THREADED)
-    #SET(MKL_NAMES ${MKL_NAMES} mkl_pgi_thread)
+if (MKL_THREADED)
 
-    IF (CMAKE_COMPILER_IS_GNUCXX)
-        SET(MKL_NAMES ${MKL_NAMES} mkl_gnu_thread)
-    ELSE (CMAKE_COMPILER_IS_GNUCXX)
-        SET(MKL_NAMES ${MKL_NAMES} mkl_intel_thread)
-    ENDIF (CMAKE_COMPILER_IS_GNUCXX)
+    if (CMAKE_COMPILER_IS_GNUCXX)
+        set(MKL_NAMES ${MKL_NAMES} mkl_gnu_thread)
+    else (CMAKE_COMPILER_IS_GNUCXX)
+        set(MKL_NAMES ${MKL_NAMES} mkl_intel_thread)
+    endif (CMAKE_COMPILER_IS_GNUCXX)
+    # For pgi compiler use set(MKL_NAMES ${MKL_NAMES} mkl_pgi_thread)
 
-    FIND_PACKAGE(OpenMP REQUIRED)
-    LIST(APPEND CMAKE_CXX_FLAGS ${OpenMP_CXX_FLAGS})
-    LIST(APPEND CMAKE_C_FLAGS ${OpenMP_C_FLAGS})
+    find_package(OpenMP REQUIRED)
+    list(APPEND CMAKE_CXX_FLAGS ${OpenMP_CXX_FLAGS})
+    list(APPEND CMAKE_C_FLAGS ${OpenMP_C_FLAGS})
 
-    # ?? might need some of this later ??
-    #SET(MKL_NAMES ${MKL_NAMES} iomp5) # should link against this for gnu g++ probably rather than the built in openMP lib :\
-    # SET(MKL_NAMES ${MKL_NAMES} pthread) # If dont want to use openMP for threading?!
-    # Threading library for dynamic linking (needed if use the compilers own omp or pthreads?)
-    #SET(MKL_NAMES ${MKL_NAMES} guide)
+else (MKL_THREADED)
 
-ELSE (MKL_THREADED)
-    SET(MKL_NAMES ${MKL_NAMES} mkl_sequential)
-ENDIF (MKL_THREADED)
+    set(MKL_NAMES ${MKL_NAMES} mkl_sequential)
 
-FOREACH (MKL_NAME ${MKL_NAMES})
-  FIND_LIBRARY(${MKL_NAME}_LIBRARY
-    NAMES ${MKL_NAME}
-    PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib
-          /opt/intel/mkl/*/lib/*/
-          /opt/intel/Compiler/*/*/mkl/lib/*/
+endif (MKL_THREADED)
+
+# Loop over required library names adding to MKL_LIBRARIES.
+foreach (MKL_NAME ${MKL_NAMES})
+
+    find_library(${MKL_NAME}_LIBRARY
+        NAMES ${MKL_NAME}
+        PATHS
+        /usr/lib64
+        /usr/lib
+        /usr/local/lib64
+        /usr/local/lib
+        /opt/intel/mkl/*/lib/*/
+        /opt/intel/Compiler/*/*/mkl/lib/*/
     )
 
-  SET(TMP_LIBRARY ${${MKL_NAME}_LIBRARY})
+    set(TMP_LIBRARY ${${MKL_NAME}_LIBRARY})
 
-  IF(TMP_LIBRARY)
-    SET(MKL_LIBRARIES ${MKL_LIBRARIES} ${TMP_LIBRARY})
-  ENDIF(TMP_LIBRARY)
-ENDFOREACH(MKL_NAME)
+    if (TMP_LIBRARY)
+        set(MKL_LIBRARIES ${MKL_LIBRARIES} ${TMP_LIBRARY})
+    endif(TMP_LIBRARY)
 
-# handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
+endforeach  (MKL_NAME)
+
+
+find_path(MKL_INCLUDE_DIR mkl.h
+    PATHS
+    /usr/include/
+    /opt/intel/mkl/*/include/*/
+    /opt/intel/Compiler/*/*/mkl/include/
+)
+
+# Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(MKL DEFAULT_MSG MKL_LIBRARIES)
 
-MARK_AS_ADVANCED(MKL_LIBRARY)
+# Hide the MKL_LIBRARY temporary variable in the cmake cache.
+mark_as_advanced(MKL_LIBRARY)
