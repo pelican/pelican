d346eed9ab8977c539161c51fee730a0b010c8c2
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-04-13 18:24:02 +0100

    Cleaned up and documented new ChunkerFactory.

--- pelican/server/src/ChunkerFactory.cpp
+++ pelican/server/src/ChunkerFactory.cpp
@@ -1,43 +1,38 @@
 #include "pelican/server/ChunkerFactory.h"
-#include "pelican/server/ChunkerClassGeneratorBase.h"
-#include <QtGlobal>
 
 #include "pelican/utility/memCheck.h"
 
 namespace pelican {
 
-QHash<QString, ChunkerFactory::AbstractGenerator* > ChunkerFactory::AbstractGenerator::_types;
-
-/**
- * @details ChunkerFactory
- */
-ChunkerFactory::ChunkerFactory(const Config* config, const Config::TreeAddress& base)
-    : AbstractFactory(config, base)
-{
-}
+QHash<QString, ChunkerFactory::AbstractCreator* > ChunkerFactory::AbstractCreator::_types;
 
 /**
  * @details
+ * Destroys the chunker factory and deletes any chunkers created by it.
  */
 ChunkerFactory::~ChunkerFactory()
 {
-    foreach (AbstractChunker* chunker, _objects) {
-        delete chunker;
+    for (int i = 0; i < _objects.size(); ++i) {
+        delete _objects[i];
     }
 }
 
 /**
  * @details
- * @param type
- * @param name
- * @return
+ * Creates a new abstract chunker of the given type, with the given
+ * name (default blank).
+ *
+ * @param[in] type The chunker type.
+ * @param[in] name The name of the chunker.
+ *
+ * @return Returns a pointer to the allocated chunker.
  */
 AbstractChunker* ChunkerFactory::create(const QString& type, const QString& name)
 {
-    if (! AbstractGenerator::types().contains(type))
-        throw QString("ChunkerFactory::create(): Unknown type \"%1\"").arg(type);
+    if (! AbstractCreator::types().contains(type))
+        throw QString("ChunkerFactory::create(): Unknown type '%1'").arg(type);
 
-    AbstractChunker* chunker = AbstractGenerator::types()[type]->create(configuration(type, name));
+    AbstractChunker* chunker = AbstractCreator::types()[type]->create(configuration(type, name));
     _objects.append(chunker);
     return chunker;
 }
