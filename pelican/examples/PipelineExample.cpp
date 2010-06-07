2aa7598523c7163be9f8298af6a9ce187446a96a
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-06-07 15:31:20 +0100

    Updated pipeline documentation.

--- pelican/examples/PipelineExample.cpp
+++ pelican/examples/PipelineExample.cpp
@@ -1,13 +1,30 @@
 #include "PipelineExample.h"
 
-
-// class PipelineExample 
-PipelineExample::PipelineExample()
-    : AbstractPipeline()
+// Initialises the pipeline, creating any modules and data blobs required,
+// and requesting any remote data.
+void PipelineExample::init()
 {
+    // Create the pipeline modules.
+    imager = (ZenithImagerDft*) createModule("ZenithImagerDft");
+    fitsWriter = (ImageWriterFits*) createModule("ImageWriterFits");
+
+    // Create local data blobs.
+    imageData = (ImageData*) createBlob("ImageData");
+
+    // Request remote data.
+    requestRemoteData("AntennaPositions");
+    requestRemoteData("VisibilityData");
 }
 
-PipelineExample::~PipelineExample()
+// Defines a single iteration of the pipeline.
+void PipelineExample::run(QHash<QString, DataBlob*>& remoteData)
 {
-}
+    // Get the remote data.
+    AntennaPositions* antPos = (AntennaPositions*)
+            remoteData["AntennaPositions"];
+    VisibilityData* visData = (VisibilityData*) remoteData["VisibilityData"];
 
+    // Run each module as required.
+    imager->run(imageData, antPos, visData);
+    fitsWriter->run(imageData);
+}
--- pelican/examples/PipelineExample.h
+++ pelican/examples/PipelineExample.h
@@ -2,18 +2,26 @@
 #define PIPELINEEXAMPLE_H
 
 #include "pelican/core/AbstractPipeline.h"
+#include "pelican/modules/ZenithImagerDft.h"
+#include "pelican/modules/ImageWriterFits.h"
+#include "pelican/data/ImageData.h"
 
 using namespace pelican;
 
 class PipelineExample : public AbstractPipeline
 {
+    private:
+        // Module pointers.
+        ZenithImagerDft* imager;
+        ImageWriterFits *fitsWriter;
+
+        // Local data blobs.
+        ImageData* imageData;
+
     public:
-        PipelineExample();
-        virtual ~PipelineExample();
-
+        PipelineExample() : AbstractPipeline() {}
         virtual void init();
-        virtual void run(QHash<QString, DataBlob*>& data);
-
+        virtual void run(QHash<QString, DataBlob*>& remoteData);
 };
 
 #endif // PIPELINEEXAMPLE_H 
