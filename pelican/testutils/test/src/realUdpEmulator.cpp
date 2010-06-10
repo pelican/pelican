d4179168ca58cf42d790b9942f416d1da9d02821
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-06-10 17:36:18 +0100

    Moved telescope emulator to testutils folder and renamed it to realUdpEmulator.

---
+++ pelican/testutils/test/src/realUdpEmulator.cpp
@@ -0,0 +1,34 @@
+#include "pelican/testutils/EmulatorDriver.h"
+#include "pelican/testutils/RealUdpEmulator.h"
+#include <iostream>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QString>
+
+using namespace pelican;
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+
+    // Get command line arguments.
+    if (argc != 5) {
+        std::cerr << "Usage: realUdpEmulator <port> <host> "
+                "<interval, microsec> <packet size, bytes>" << std::endl;
+        return 1;
+    }
+    QString port = QString::fromAscii(argv[1]);
+    QString host = QString::fromAscii(argv[2]);
+    QString interval = QString::fromAscii(argv[3]);
+    QString size = QString::fromAscii(argv[4]);
+
+    // Start the emulator.
+    ConfigNode emulatorConfig(
+            "<RealUdpEmulator>"
+            "    <connection host=\"" + host + "\" port=\"" + port + "\"/>"
+            "    <packet size=\"" + size + "\" interval=\"" + interval + "\"/>"
+            "</RealUdpEmulator>"
+            );
+    EmulatorDriver emulator(new RealUdpEmulator(emulatorConfig));
+
+    return app.exec();
+}
