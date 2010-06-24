4b3f16155d4ca4968b04304be3bfea7971ace7db
Benjamin Mort <benjamin.mort@oerc.ox.ac.uk>
2010-06-24 18:08:46 +0100

    added updated (broken) cmake for the new build system

--- pelican/cmake/packagemacrosNew.cmake
+++ pelican/cmake/packagemacrosNew.cmake
@@ -2,5 +2,10 @@
 set(SUBPACKAGE_WORK_DIR ${CMAKE_BINARY_DIR}/_subpackages)
 file(MAKE_DIRECTORY ${SUBPACKAGE_WORK_DIR})
 
+# Define macros for creating sub-packages.
 include(cmake/subpackage.cmake)
+
+# Defined macros for creating project libraries (from sub-packages).
 include(cmake/projectLibrary.cmake)
+
+
--- pelican/cmake/projectLibrary.cmake
+++ pelican/cmake/projectLibrary.cmake
@@ -1,54 +1,60 @@
 
+# Create the project library specfied by the name argument.
+macro(CREATE_PROJECT_LIBRARY name)
 
-macro(PROJECT_LIBRARY name)
+    # Die horribly if the project library has not been declared.
+    if(NOT ${name}_subpackage_DEPS)
+        message(FATAL_ERROR "Trying to build a project library '${name}' " 
+            "which has not been declared.")
+    endif(NOT ${name}_subpackage_DEPS)
 
     ## write project library file for linking modules in single library mode.
 
-    if(BUILD_SINGLE_LIB)
-        #message(STATUS "++ ${name}")
-        
-            # Set the name of the sub-package file.
-        set(project_lib_file "${SUBPACKAGE_WORK_DIR}/_${name}.cmake")
-
-        file(WRITE "${project_lib_file}"
-            "set(${name}_dependencies ${ARGN})\n\n")
     
-        ## link subpackage objects into single library
-        #---------------------------------------------
-        
+    # Only proceed if in single library mode.
+    if(BUILD_SINGLE_LIB)
+    
+        # Load the project library file to get objects / includes / link deps.    
+    
+            # Set the name of the sub-package file.
+        #set(project_file "${SUBPACKAGE_WORK_DIR}/_${name}.cmake")
+
         # Reset shared object list. 
-        set(shared_objects "")
+        #set(shared_objects "")
         
-        # Load the shared objects for the single library target.
-        foreach(subpackage ${ARGN})
-            #message(STATUS "     ${subpackage}")
-            include("${SUBPACKAGE_WORK_DIR}/${subpackage}.cmake")
-            list(APPEND subpackage_external_libs ${subpackage_${subpackage}_external_LIBS})
-        endforeach(subpackage)
+        # Load the project file.
+        #include(${project_file})
+
+        # Mark objects as generated ## why?               
+        #foreach(obj ${shared_objects}) 
+#            set_source_files_properties(${obj} PROPERTIES GENERATED 1)
+        #endforeach(obj)
         
-        list(REMOVE_DUPLICATES subpackage_external_libs)
+        # Create a target for the project library.
+        #add_library("${name}" SHARED ${shared_objects})
+        #set_target_properties("${name}" PROPERTIES LINKER_LANGUAGE CXX)
+        #add_dependencies("${name}" ${shared_libs})
         
-        file(APPEND "${project_lib_file}"
-            "set(${name}_shared_objs ${shared_objects})\n\n")
-        file(APPEND "${project_lib_file}"
-            "set(${name}_libs ${subpackage_external_libs})\n\n")
+        # Add an install target for the project library.        
+        #install(TARGETS "${name}" DESTINATION ${LIBRARY_INSTALL_DIR})
         
-        foreach(obj ${shared_objects}) 
-            #message(STATUS "*** ${obj}")
-            set_source_files_properties(${obj} PROPERTIES GENERATED 1)
-        endforeach(obj)
-        
-        add_library("${name}" SHARED ${shared_objects})
-        set_target_properties("${name}" PROPERTIES LINKER_LANGUAGE CXX)
-        
-        add_dependencies("${name}" ${shared_libs})
-        install(TARGETS "${name}" DESTINATION ${LIBRARY_INSTALL_DIR})
-        
-        target_link_libraries("${name}" ${subpackage_external_libs})
+        # Link the project library with external library dependencies.
+        #target_link_libraries("${name}" ${subpackage_external_libs})
         
     endif(BUILD_SINGLE_LIB)
 
-endmacro(PROJECT_LIBRARY)
+endmacro(CREATE_PROJECT_LIBRARY)
 
 
+
+# Declare a project library to be build from the specified subpackages.
+macro(DECLARE_PROJECT_LIBRARY name)
+    set(${name}_subpackage_DEPS ${ARGN})
+    list(APPEND project_libraries ${name})
+    
+    file(WRITE "${SUBPACKAGE_WORK_DIR}/_${name}.cmake"
+        "# Project library '${name}' - this file is auto-generated "
+        "do not edit!\n\n"
+    )
+endmacro(DECLARE_PROJECT_LIBRARY)
 
