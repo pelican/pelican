5fb822df73f541834fd05e20a9761ca33d247c0a
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2010-08-09 18:14:41 +0100

    add FileChunker to monitor and read in files as they change - untested

---
+++ pelican/utility/src/WatchedFile.cpp
@@ -0,0 +1,56 @@
+#include "pelican/utility/WatchedFile.h"
+#include <QtGlobal>
+
+#include "pelican/utility/memCheck.h"
+
+namespace pelican {
+
+
+/**
+ * @details Constructs a WatchedFile object.
+ */
+WatchedFile::WatchedFile(const QString& name, QObject* parent)
+    : QFile(name, parent)
+{
+    _init();
+}
+
+WatchedFile::WatchedFile(QObject* parent)
+    : QFile( parent)
+{
+    _init();
+}
+
+void WatchedFile::_init()
+{
+    connect( &_watcher, SIGNAL( fileChanged(const QString&) ),
+             this,  SLOT(_fileChanged(const QString &) ),
+                                  Qt::DirectConnection);
+    if( fileName() != QString() )
+    {
+        _watcher.addPath( fileName() );
+    }
+}
+
+
+/**
+ * @details Destroys the WatchedFile object.
+ */
+WatchedFile::~WatchedFile()
+{
+}
+
+void WatchedFile::_fileChanged(const QString& )
+{
+    emit readyRead();
+}
+
+void WatchedFile::setFileName(const QString& name)
+{
+    if( fileName() != QString() )
+        _watcher.removePath( fileName() );
+    QFile::setFileName(name);
+    _watcher.addPath( fileName() );
+}
+
+} // namespace pelican
