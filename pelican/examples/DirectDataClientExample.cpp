f93b07f9dbbe7729c509b4e2f68ddf3c36176640
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2010-09-22 13:16:23 +0100

    doc: add information about the DataClient and how to insert Chunkers

---
+++ pelican/examples/DirectDataClientExample.cpp
@@ -0,0 +1,23 @@
+#include "DirectDataClientExample.h"
+#include "ChunkerExample.h"
+
+
+using namespace pelican;
+
+/**
+ * @details Constructs a DirectDataClientExample object.
+ */
+DirectDataClientExample::DirectDataClientExample(const ConfigNode& configNode,
+                const DataTypes& types, const Config* config)
+    : DirectStreamDataClient(configNode, types, config)
+{
+    addChunker("Stream1", "ChunkerExample", "Stream1ConfigName");
+    addChunker("Stream2", "ChunkerExample", "Stream2ConfigName");
+}
+
+/**
+ * @details Destroys the DirectDataClientExample object.
+ */
+DirectDataClientExample::~DirectDataClientExample()
+{
+}
