47ca5e53e0bb7f71bc8ac856a8e1b8cd79c7de60
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2010-11-08 14:21:20 +0000

    Fixed tutorial to build and run with correct XML configuration.

--- pelican/examples/tutorial/src/mainPipeline.cpp
+++ pelican/examples/tutorial/src/mainPipeline.cpp
@@ -2,22 +2,33 @@
 #include "tutorial/SignalProcessingPipeline.h"
 #include <QtCore/QCoreApplication>
 
+// Include any headers that are referenced by name.
+#include "tutorial/OutputStreamExample.h"
+#include "tutorial/SignalDataAdapter.h"
+
 int main(int argc, char* argv[])
 {
     // Create a QCoreApplication.
     QCoreApplication app(argc, argv);
 
-    // Create a PipelineApplication.
-    PipelineApplication pApp(argc, argv);
+    try {
+        // Create a PipelineApplication.
+        PipelineApplication pApp(argc, argv);
 
-    // Register the pipelines that can run.
-    pApp.registerPipeline(new SignalProcessingPipeline);
+        // Register the pipelines that can run.
+        pApp.registerPipeline(new SignalProcessingPipeline);
 
-    // Set the data client.
-    pApp.setDataClient("PelicanServerClient");
+        // Set the data client.
+        pApp.setDataClient("PelicanServerClient");
 
-    // Start the pipeline driver.
-    pApp.start();
+        // Start the pipeline driver.
+        pApp.start();
+    }
+
+    // Catch any error messages from Pelican.
+    catch (const QString& err) {
+        std::cerr << "Error: " << err.toStdString() << std::endl;
+    }
 
     return 0;
 }
