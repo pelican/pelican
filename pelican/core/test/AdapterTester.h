59c0a9270fbabda83d7401c0e4c244bb7eb2e3bc
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2011-04-18 16:36:19 +0100

    add AdapterTester unit test helper class

---
+++ pelican/core/test/AdapterTester.h
@@ -0,0 +1,62 @@
+#ifndef ADAPTERTESTER_H
+#define ADAPTERTESTER_H
+
+#include <QString>
+#include <QHash>
+#include "pelican/utility/FactoryConfig.h"
+#include "pelican/utility/Config.h"
+#include "pelican/core/AbstractAdapter.h"
+
+class QIODevice;
+
+/**
+ * @file AdapterTester.h
+ */
+
+namespace pelican {
+class DataBlob;
+namespace test {
+
+
+/**
+ * @class AdapterTester
+ * 
+ * @brief
+ *     Helper for writing Unit tests for Adapters
+ * @details
+ * 
+ */
+class AdapterTester
+{
+    public:
+        /// AdapterTester constructor.
+        AdapterTester(const QString& name, const QString& xml);
+
+        /// AdapterTester destructor.
+        virtual ~AdapterTester();
+
+        /// set a data file from which to read in test data
+        //  the file is assumed to exist in the data directory
+        //  of the test folder
+        void setDataFile(const QString& filename);
+
+        /// set any supporting service data
+        void setServiceData(QHash<QString, DataBlob*>& serviceData);
+
+        /// execute
+        //  run deserialise() passing the set data stream 
+        //  with the number of specified bytes
+        virtual void execute(DataBlob* blob, std::size_t bytes);
+
+    private:
+        FactoryConfig<AbstractAdapter>* _adapterFactory;
+        Config  _config;
+        QIODevice* _device;
+        QHash<QString, DataBlob*> _service;
+        AbstractAdapter* _object;
+};
+
+} // namespace pelican
+} // namespace pelican
+
+#endif // ADAPTERTESTER_H 
---
+++ pelican/core/test/src/AdapterTester.cpp
@@ -0,0 +1,69 @@
+#include <QFile>
+
+#include "AdapterTester.h"
+#include "pelican/core/AbstractStreamAdapter.h"
+#include "pelican/core/AbstractServiceAdapter.h"
+
+
+namespace pelican {
+namespace test {
+
+
+/**
+ * @details Constructs a AdapterTester object.
+ */
+AdapterTester::AdapterTester(const QString& type, const QString& config)
+   : _device(0)
+{
+    // Setup the XML configuration.
+    Config::TreeAddress dataAddress, adapterBase;
+    adapterBase << Config::NodeId("adapters", "");
+    QString xml = 
+            "<adapters><" + type + ">" 
+          + config + "\n</" + type + ">\n</adapters>";
+    _config.setXML(xml);
+    _adapterFactory = new FactoryConfig<AbstractAdapter>(&_config, adapterBase );
+    _object = _adapterFactory->create(type,"");
+}
+
+/**
+ * @details Destroys the AdapterTester object.
+ */
+AdapterTester::~AdapterTester()
+{
+     delete _device;
+     delete _adapterFactory; // _object gets deleted with factory
+}
+
+void AdapterTester::setDataFile(const QString& filename)
+{
+     if( _device ) 
+        delete _device;
+     _device = new QFile(filename);
+     if( ! _device->open( QIODevice::ReadOnly) ) {
+        throw( QString("cannot open file" + filename + " for reading") );
+     }
+}
+
+void AdapterTester::setServiceData(QHash<QString, DataBlob*>& serviceData)
+{
+    _service = serviceData;
+}
+
+void AdapterTester::execute(DataBlob* blob, std::size_t bytes)
+{
+     switch( _object->type() ) {
+         case AbstractAdapter::Stream:
+             ((AbstractStreamAdapter*)(_object))->config(blob, bytes, _service);
+             break;
+         case AbstractAdapter::Service:
+             ((AbstractServiceAdapter*)_object)->config(blob, bytes);
+             break;
+         default:
+             throw( QString("unknown adapter type :%1").arg( _object->type()) ) ;
+     }
+     _object->deserialise(_device);
+}
+
+} // test
+} // namespace pelican
