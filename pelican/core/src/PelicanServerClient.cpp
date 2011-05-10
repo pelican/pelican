4f9b4343ca204fbc9ec2abb6c85692d460ef7a91
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2011-05-10 15:31:11 +0100

    improve message

--- pelican/core/src/PelicanServerClient.cpp
+++ pelican/core/src/PelicanServerClient.cpp
@@ -118,7 +118,8 @@ AbstractDataClient::DataBlobHash PelicanServerClient::_sendRequest(
     sock.connectToHost(_server, _port , QIODevice::ReadWrite);
     if(! sock.waitForConnected(-1))
     {
-        throw(QString("PelicanServerClient: unable to connect to host ") + _server + " port " +_port);
+        throw(QString("PelicanServerClient: unable to connect to host ") + _server 
+            + QString(" port %1").arg( _port) );
     }
 
     // Write the request to the open TCP socket with the PelicanClientProtocol.
