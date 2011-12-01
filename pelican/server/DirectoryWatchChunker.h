77bc33a9cc2f3a543f682a220cd7a271db352b70
Chris Williams <christopher.williams@oerc.ox.ac.uk>
2011-12-01 14:25:57 +0000

    new chunker: watch for new files that appear in a directory

---
+++ pelican/server/DirectoryWatchChunker.h
@@ -0,0 +1,38 @@
+#ifndef DIRECTORYWATCHCHUNKER_H
+#define DIRECTORYWATCHCHUNKER_H
+
+
+#include "AbstractChunker.h"
+
+/**
+ * @file DirectoryWatchChunker.h
+ */
+
+namespace pelican {
+
+/**
+ * @class DirectoryWatchChunker
+ *  
+ * @brief
+ *    Monitors a directory for new files
+ * @details
+ *    Watches a Directory, As new files appear they are
+ *    sent as a new chunk
+ * 
+ */
+
+class DirectoryWatchChunker : public AbstractChunker
+{
+    public:
+        DirectoryWatchChunker( const ConfigNode& config );
+        ~DirectoryWatchChunker();
+
+        virtual QIODevice* newDevice();
+        virtual void next(QIODevice*);
+
+    private:
+        QString _dirName;
+};
+
+} // namespace pelican
+#endif // DIRECTORYWATCHCHUNKER_H 
---
+++ pelican/server/src/DirectoryWatchChunker.cpp
@@ -0,0 +1,53 @@
+#include "DirectoryWatchChunker.h"
+#include "pelican/utility/WatchedDir.h"
+
+
+namespace pelican {
+
+
+/**
+ *@details DirectoryWatchChunker 
+ */
+DirectoryWatchChunker::DirectoryWatchChunker( const ConfigNode& config )
+    : AbstractChunker(config)
+{
+    if( config.getDomElement().isNull() )
+    {
+        throw( QString("DirectoryWatchChunker: empty config passed in constructor") );
+    }
+    if( ! config.hasAttribute("dir") )
+        throw( QString("FileChunker: no \"dir\" attribute specified" ));
+    _dirName = config.getAttribute("dir");
+    if( _dirName == "" )
+        throw( QString("FileChunker: no dirname specified" ));
+}
+
+/**
+ *@details
+ */
+DirectoryWatchChunker::~DirectoryWatchChunker()
+{
+}
+
+QIODevice* DirectoryWatchChunker::newDevice() {
+    WatchedDir* device = new WatchedDir(_dirName);
+    if( ! device->open(QIODevice::ReadOnly) )
+        throw(QString("DirectoryWatchChunker: unable to open file:%1").arg(_dirName));
+    return device;
+}
+
+void DirectoryWatchChunker::next(QIODevice* dev) {
+    // get the new file
+    WatchedDir* file=static_cast<WatchedDir*>(dev);
+
+    // Get writable buffer space for the chunk.
+    WritableData writableData = getDataStorage( file->size() );
+    if( writableData.isValid() ) {
+        // Get pointer to start of writable memory.
+        char* ptr = (char*) (writableData.ptr());
+        file->readFile( ptr, file->size() );
+    }
+}
+PELICAN_DECLARE(AbstractChunker, DirectoryWatchChunker )
+
+} // namespace pelican
